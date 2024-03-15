8479e251b8fa5ef42834d37bf63ca1fc6970a7bb
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-03-15 17:50:43 +0200

    Delete Subqueries and JOINs/01. Towns Addresses.sql

--- Subqueries and JOINs/01. Towns Addresses.sql
+++
@@ -1,9 +0,0 @@
-SELECT	
-	T.town_id,
-    T.name,
-    A.address_text
-from towns as T
-join addresses as A
-on T.town_id = A.town_id
-where T.name = 'Sofia' or T.name = 'Carnation' or T.name = 'San Francisco'
-order by T.town_id, A.address_id;
